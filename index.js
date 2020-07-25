503409fd00e9cf3eb7e219373d730cfac974b11b
tungocvan <tungocvan@gmail.com>
2020-07-25 21:56:11 +0700

    v11

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/tin-tuc', (req, res) => res.send('Tin Tuc'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(process.env.PORT || 3000)
