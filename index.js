255f28338e425e134512b110b28dd9cb390d21a5
tungocvan <tungocvan@gmail.com>
2020-07-27 10:17:48 +0700

    v3

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/tin-tuc', (req, res) => res.send('Tin Tuc'))
+app.get('/contact', function(req, res){
+    res.send('Liên hệ')
+})
 
 app.listen(process.env.PORT || 3000)
